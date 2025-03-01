6a338ffa6b3ed8ffffc9185bcf7bb1d3fdf12437
chaeyoung103 <thdcodud103@naver.com>
2025-03-01 12:24:23 +0900

    feat: add font, images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,5 @@
+import LogoIcon from "./logo.svg?react";
+import MainOneImage from "./main1.svg?react";
+import MainTwoImage from "./main2.svg?react";
+
+export { LogoIcon, MainOneImage, MainTwoImage };
