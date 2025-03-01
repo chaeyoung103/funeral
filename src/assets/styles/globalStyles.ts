6ff8c159ff777c17c240a946033ed4265b83a889
chaeyoung103 <thdcodud103@naver.com>
2025-03-01 12:13:14 +0900

    chore: init setting (svg, path..)

---
+++ src/assets/styles/globalStyles.ts
@@ -0,0 +1,53 @@
+import { css } from "@emotion/react";
+
+export const globalStyles = css`
+  @font-face {
+    font-family: "JejuGothic";
+    src: url("/src/assets/fonts/JejuGothicOTF.otf") format("opentype");
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: "JejuMyeongjo";
+    src: url("/src/assets/fonts/JejuMyeongjoOTF.otf") format("opentype");
+    font-weight: normal;
+    font-style: normal;
+  }
+  * {
+    box-sizing: border-box;
+    font-family: "JejuGothic", "JejuMyeongjo", sans-serif;
+  }
+
+  html {
+    box-sizing: border-box;
+    font-family: "JejuGothic", "JejuMyeongjo", sans-serif;
+    font-size: 62.5%;
+  }
+
+  html,
+  body,
+  #root {
+    width: 100vw;
+    height: calc(var(--vh, 1vh) * 100);
+    margin: 0 auto;
+    background-color: white;
+
+    -webkit-overflow-scrolling: touch !important;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+
+    ::-webkit-scrollbar {
+      display: none;
+    }
+  }
+
+  button {
+    padding: 0;
+    overflow: visible;
+    cursor: pointer;
+    background: inherit;
+    border: none;
+    border-radius: 0;
+    box-shadow: none;
+  }
+`;
